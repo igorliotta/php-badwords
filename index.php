fff82bf4eca36af38e14e5a823b2b0210a40038c
igorliotta <liottaigor@gmail.com>
2023-11-21 15:14:20 +0100

    Added: form in index.php and text in pharagraph.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Filtraggio</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="pharagraph.php" method="post">
         <input type="text" name="censured_word" placeholder="Parola da censurare">
         <input type="submit" value="Invia">
     </form>
---
+++ pharagraph.php
@@ -0,0 +1,25 @@
+<?php 
+
+$text = 'Nel 1971, il teorema dell’area di Hawking diede il via a una serie di intuizioni fondamentali sulla meccanica dei buchi neri e suggerì un curioso parallelo con la seconda legge della termodinamica, che afferma che l’entropia – o grado di disordine all’interno di un sistema – non dovrebbe mai diminuire. La somiglianza tra le due teorie suggeriva che i buchi neri potessero comportarsi come oggetti termici che emettono calore – una proposta sconcertante, poiché si pensava che i buchi neri per loro stessa natura non lasciassero mai fuoriuscire energia. Nel 1974, Hawking mostrò che i buchi neri potrebbero emettere radiazioni su scale temporali molto lunghe se fossero presi in considerazione i loro effetti quantistici. Questo fenomeno è conosciuto come radiazione di Hawking e rimane una delle rivelazioni più importanti sui buchi neri. Da allora Hawking e altri scienziati hanno dimostrato che il teorema dell’area funziona matematicamente, ma non c’era modo di verificarlo in natura fino alla prima rilevazione delle onde gravitazionali di Ligo.';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testo</title>
+</head>
+<body>
+
+  <h3>L’AREA DELL’ORIZZONTE DEGLI EVENTI NON SI RIDUCE</h3>
+  <h1>Confermato un teorema di Hawking sui buchi neri</h1>
+  <p><?php echo $text; ?></p>
+
+</body>
+</html>
+
+
+
+
