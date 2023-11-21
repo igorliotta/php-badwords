de7d2d80a289226a17985151c0f35becec7918a8
igorliotta <liottaigor@gmail.com>
2023-11-21 14:58:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtraggio</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="censured_word" placeholder="Parola da censurare">
+        <input type="submit" value="Invia">
+    </form>
+</body>
+</html>
